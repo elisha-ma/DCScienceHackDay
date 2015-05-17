51047746fc20ff290893026ed8a65e5729edd73b
Elisha Ma <elisma@cisco.com>
2015-05-17 10:20:19 -0400

    itemgetter

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from operator import itemgetter
 from .models import Stars
 
 import requests
