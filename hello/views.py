6ed8f2b3df80b5869b0dcd6ac90899f8de79aa53
Elisha Ma <elisma@cisco.com>
2015-05-17 10:08:15 -0400

    added model import back

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
-#from .models import Stars
+from .models import Stars
 
 import requests
 import os
