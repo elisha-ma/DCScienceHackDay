f07327b3e6e65009d6b2c5bcf8fef6f7278714bd
Elisha Ma <elisma@cisco.com>
2015-05-17 12:25:03 -0400

    rotated again

--- hello/views.py
+++ hello/views.py
@@ -78,6 +78,7 @@ def calc_view(ra1,dec1,dist,ra2,dec2,tilt,star_list, in_const):
     star_cart = [zrotate(ra2)*x for x in star_cart]
     star_cart = [yrotate(dec2)*x for x in star_cart]
     star_cart = [xrotate(tilt)*x for x in star_cart]
+    star_cart = [yrotate(-scipy.pi/2)*x for x in star_cart]
     
     star_sphere = [cart2sphere(x.item(0),x.item(1),x.item(2)) for x in star_cart]  # transform back to spherical
     
