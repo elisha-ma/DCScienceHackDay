82e50e2bf4f641c73b225acb619a45062c7816be
Elisha Ma <elisma@cisco.com>
2015-05-17 10:03:09 -0400

    workinggit add hello/

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
-from .models import Stars
+#from .models import Stars
 
 import requests
 import os
@@ -16,20 +16,21 @@ def index(request):
     return render(request, 'stars.html', {})
 
 def input(request):
-    ra1 = request.POST.get('ra1')
-    dec1 = request.POST.get('dec1')
-    dist = request.POST.get('dist')
-    ra2 = request.POST.get('ra2')
-    dec2 = request.POST.get('dec2')    
-    tilt = request.POST.get('tilt')
+    ra1 = float(request.POST.get('ra1')) * scipy.pi / 180
+    dec1 = float(request.POST.get('dec1')) * scipy.pi / 180
+    dist = float(request.POST.get('dist'))
+    ra2 = float(request.POST.get('ra2')) * scipy.pi / 180
+    dec2 = float(request.POST.get('dec2')) * scipy.pi / 180
+    tilt = float(request.POST.get('tilt')) * scipy.pi / 180
     
-    #star = Stars.objects.get(starid=1)
-    star_list = __get_database()
+    #star_list = __get_database()
     
-    [x,y,z] = sphere2cart(dist,ra1,dec1)
+    star_disp = calc_view(ra1,dec1,dist,ra2,dec2,tilt,[[1,2,3,0]])
+    coordinates_list = adjust_for_image(star_disp)
     
-    coordinates_list = [(0, 0), (50, 50), (100, 100)]
-    context = {'ra1':x, 'dec1':y, 'dist':z, 'ra2':ra2, 'dec2':dec2, 'tilt':tilt, "coordinates_list":coordinates_list} 
+    #coordinates_list = [(0, 0), (50, 50), (100, 100)]
+    #context = {'ra1':ra1, 'dec1':dec1, 'dist':dist, 'ra2':ra2, 'dec2':dec2, 'tilt':tilt, "coordinates_list":coordinates_list} 
+    context = {"coordinates_list":coordinates_list}
     return render(request, 'result.html', context)
 
 def __get_database():
@@ -43,40 +44,48 @@ def __get_database():
 
     return star_list
 
+def adjust_for_image(star_disp):
+    visible_flux = .01
+    visible_rgb = 10
+    return [[x[0] * 256 + 256, 256 - x[1] * 256, scipy.int8(scipy.minimum(x[2]/visible_flux*visible_rgb,255)) ] for x in star_disp]
+
 def calc_view(ra1,dec1,dist,ra2,dec2,tilt,star_list):
-    phi_view = 45
+    phi_view = 45 * scipy.pi / 180
     
     [xshift,yshift,zshift] = sphere2cart(dist,ra1,dec1)     # calculate cartesian shift
     star_cart = [sphere2cart(x[2],x[0],x[1]) for x in star_list]    # calculate star cartesian from spherical
     star_cart = [[x[0]-xshift, x[1]-yshift, x[2]-zshift] for x in star_cart]   # perform cartesian shift
+    star_cart = [scipy.transpose(scipy.matrix(x)) for x in star_cart]
     
-    star_cart = [scipy.transpose(zrotate(ra2)*scipy.array([[x[0]], [x[1]], [x[2]]])) for x in star_cart]    # Orientation Rotations
-    star_cart = [scipy.transpose(yrotate(dec2)*scipy.array([[x[0]], [x[1]], [x[2]]])) for x in star_cart]
-    star_cart = [scipy.transpose(xrotate(tilt)*scipy.array([[x[0]], [x[1]], [x[2]]])) for x in star_cart]
+    #star_cart = [scipy.array(scipy.transpose(zrotate(ra2)*scipy.transpose(scipy.matrix(x)))) for x in star_cart]
+    #star_cart = [scipy.array(scipy.transpose(zrotate(ra2)*scipy.matrix([[x[0]], [x[1]], [x[2]]]))) for x in star_cart]  # Orientation Rotations
+    star_cart = [zrotate(ra2)*x for x in star_cart]
+    star_cart = [yrotate(dec2)*x for x in star_cart]
+    star_cart = [xrotate(tilt)*x for x in star_cart]
     
-    star_sphere = [cart2sphere(x[0],x[1],x[2]) for x in star_cart]  # transform back to spherical
+    star_sphere = [cart2sphere(x.item(0),x.item(1),x.item(2)) for x in star_cart]  # transform back to spherical
     
     star_disp = [];
     for ind in range(len(star_list)):
-        if star_sphere[ind][2] > (90-phi_view):
-            rdisp = (90-star_sphere[ind][2])/phi_view
+        if star_sphere[ind][2] > (90 * scipy.pi / 180 - phi_view):
+            rdisp = (90 * scipy.pi / 180 - star_sphere[ind][2])/phi_view
             thetadisp = star_sphere[ind][1]
             xdisp = rdisp*scipy.cos(thetadisp)
             ydisp = rdisp*scipy.sin(thetadisp)
-            bright = scipy.log10(-star_list[ind][3]/2.5)*scipy.power(star_list[ind][0],2)/scipy.power(star_sphere[ind][0],2)
+            bright = scipy.power(10,star_list[ind][3]/-2.5)*scipy.power(star_list[ind][0],2)/scipy.power(star_sphere[ind][0],2)
             star_disp.append([xdisp, ydisp, bright])
     
     return star_disp
         
     
 def xrotate(theta):
-    return scipy.array([[1, 0, 0], [0, scipy.cos(theta), -scipy.sin(theta)], [0, scipy.sin(theta), scipy.cos(theta)]])
+    return scipy.matrix([[1, 0, 0], [0, scipy.cos(theta), -scipy.sin(theta)], [0, scipy.sin(theta), scipy.cos(theta)]])
 
 def yrotate(theta):
-    return scipy.array([[scipy.cos(theta), 0, -scipy.sin(theta)], [0, 1, 0], [scipy.cos(theta), 0, scipy.sin(theta)]])    
+    return scipy.matrix([[scipy.cos(theta), 0, -scipy.sin(theta)], [0, 1, 0], [scipy.sin(theta), 0, scipy.cos(theta)]])    
 
 def zrotate(theta):
-    return scipy.array([[scipy.cos(theta), -scipy.sin(theta), 0], [scipy.sin(theta), scipy.cos(theta), 0], [0, 0, 1]])
+    return scipy.matrix([[scipy.cos(theta), -scipy.sin(theta), 0], [scipy.sin(theta), scipy.cos(theta), 0], [0, 0, 1]])
     
 def sphere2cart(r,theta,phi):
     x = r*scipy.cos(theta)*scipy.cos(phi)
@@ -85,7 +94,7 @@ def sphere2cart(r,theta,phi):
     return [x, y, z]
 
 def cart2sphere(x,y,z):
-    r = scipy.sqrt(scipy.power(x,2)+scipy.power(y,2)+scipy.power(z,2))
+    r = scipy.real(scipy.sqrt(scipy.power(x,2)+scipy.power(y,2)+scipy.power(z,2)))
     phi = scipy.arcsin(z/r)
     theta = scipy.arctan2(y,x)
     return [r, theta, phi]
